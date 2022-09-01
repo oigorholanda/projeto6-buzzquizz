f632372c43f3a895cec8cb5ee9a3ed2bbbde36a5
Marcus Vinícius de Castro Rodrigues <marcus.castro0701@gmail.com>
2022-09-01 12:09:16 -0300

    layout tela 3 (criaçao do quizz) e o início do js

---
+++ script.js
@@ -0,0 +1,32 @@
+function expande(fator){
+    const a = document.querySelector('.tampa1')
+    a.classList.toggle('none')
+    const b = document.querySelector('.tampa2')
+    b.classList.toggle('none')
+    const c = document.querySelector('.tampa3')
+    c.classList.toggle('none')
+    const d = document.querySelector('.primeirapergunta')
+    d.classList.toggle('margin-bottom')
+}
+
+function expande2(){
+    const a = document.querySelector('.tampa4')
+    a.classList.toggle('none')
+    const b = document.querySelector('.tampa5')
+    b.classList.toggle('none')
+    const c = document.querySelector('.tampa6')
+    c.classList.toggle('none')
+    const d = document.querySelector('.segundapergunta')
+    d.classList.toggle('margin-bottom')
+}
+
+function expande3(){
+    const a = document.querySelector('.tampa7')
+    a.classList.toggle('none')
+    const b = document.querySelector('.tampa8')
+    b.classList.toggle('none')
+    const c = document.querySelector('.tampa9')
+    c.classList.toggle('none')
+    const d = document.querySelector('.terceirapergunta')
+    d.classList.toggle('margin-bottom')
+}
